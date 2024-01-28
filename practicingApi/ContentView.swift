194ac4f437aa1e7ba61a9bfddfa284cadb2a908d
joaobug <joaoenfrance@gmail.com>
2024-01-28 14:52:47 -0300

    Add structures fto represent returned data Parse the response json Return businesses to content view

--- practicingApi/ContentView.swift
+++ practicingApi/ContentView.swift
@@ -33,7 +33,7 @@ struct ContentView: View {
             }
                 .padding()
                 .task {
-                    await service.businessSearch()
+                    let businesses = await service.businessSearch()
                 }
             
         }
--- practicingApi/DataService.swift
+++ practicingApi/DataService.swift
@@ -11,11 +11,11 @@ struct DataService {
     
     let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
     
-    func businessSearch() async {
+    func businessSearch() async -> [Business] {
         
         // check if api key exists
         guard apiKey != nil else {
-            return
+            return [Business]()
         }
         
         
@@ -28,12 +28,16 @@ struct DataService {
             // 3. send request
             do{
                let(data, response) = try await URLSession.shared.data(for: request)
-                print(data)
-                print(response)
+                
+                // 4. parse json
+                let decoder = JSONDecoder()
+               let result = try decoder.decode(BusinessSearch.self, from: data)
+                return result.businesses
             }
             catch {
                 print(error)
             }
         }
+        return [Business]()
     }
 }
